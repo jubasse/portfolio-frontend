8a7654ba085aa7fd228d324f1e51f0d84575f930
Julien METRAL <contact@julien-metral.fr>
2023-09-24 23:05:48 +0400

    feat: Add tailwind css and postcss, rename App into Profile

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { ProfileContextProvider } from './contexts';
-
-const App: FC = () => {
-  return <ProfileContextProvider></ProfileContextProvider>;
-};
-
-export default App;
