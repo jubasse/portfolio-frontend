76889f8e57383f16a830c470d21b0572cc6a9b18
Julien METRAL <contact@julien-metral.fr>
2023-10-08 23:47:20 +0400

    feat: Handle dark mode in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
